369da602e06f7e366a1174d49f34a8c33a50e588
MSulaimanKhalid <muhammadsulaimankhalid5566@gmail.com>
2025-02-25 17:36:35 +0500

    Add files via upload

---
+++ Week 3/Login.java
@@ -0,0 +1,9 @@
+public class Login{
+	private static String username="1234", password="1234";
+	static String getUsername(){
+		return username;
+	}
+	static String getPassword(){
+		return password;
+	}
+}
